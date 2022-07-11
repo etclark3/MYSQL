0b362c6675c3a7e0aef14c2420fa6b1fba31bdd3
Everett Clark <everetttylerclark@gmail.com>
2022-07-11 10:37:15 -0500

    Completed

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -2,7 +2,6 @@
 SELECT DISTINCT title 
 FROM titles;
 	-- QA: 7 
-SELECT * FROM employees;
 
 -- Q2: Find a list of all unique last names of all employees that start and end with 'E' using GROUP BY.
 SELECT last_name 
@@ -42,37 +41,77 @@ GROUP BY first_name, gender;
     
 -- Q7: Generate a count employees for each unique username. Are there any duplicate usernames? BONUS: How many duplicate usernames are there?
 SELECT
-	LOWER(
-    CONCAT(
-	SUBSTR(first_name, 1,1)
-    ,SUBSTR(last_name, 1,4)
-    ,'_'
-    ,SUBSTR(birth_date, 6,2)
-    ,SUBSTR(birth_date, 3,2)
-    )) AS username,
+LOWER(
+CONCAT(
+SUBSTR(first_name, 1,1),
+SUBSTR(last_name, 1,4),'_',
+SUBSTR(birth_date, 6,2),
+SUBSTR(birth_date, 3,2)
+)) AS username,
 COUNT(*) AS num
 FROM employees
 GROUP BY username
 ORDER BY num DESC;
 	-- QA: There are many duplicate usernames
     
--- Q8: Bonus: More practice with aggregate functions:
-    -- Q8A: Determine the historic average salary for each employee. When you hear, read, or think "for each" with regard to SQL, you'll probably be grouping by that exact column.
+-- Q8: Bonus: More practice with aggregate functions: --
+   
+   -- Q8A: Determine the historic average salary for each employee.
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS Employee, ROUND(AVG(salary))
+FROM salaries
+JOIN employees
+USING (emp_no)
+GROUP BY emp_no;
  
     -- Q8B: Using the dept_emp table, count how many current employees work in each department. The query result should show 9 rows, one for each department and the employee count.
-  
+SELECT dept_no, dept_name AS Department, COUNT(*)
+FROM dept_emp
+JOIN departments
+USING (dept_no)
+WHERE to_date > NOW()
+GROUP BY dept_name
+ORDER BY dept_no;
+
     -- Q8C: Determine how many different salaries each employee has had. This includes both historic and current.
-	
+SELECT emp_no, count(salary)
+FROM salaries
+GROUP BY emp_no;
+
     -- Q8D: Find the maximum salary for each employee.
-	
+SELECT CONCAT(first_name, ' ', last_name) AS Employee, emp_no, MAX(salary)
+FROM salaries
+JOIN employees
+USING (emp_no)
+GROUP BY emp_no;
+
     -- Q8E: Find the minimum salary for each employee.
-	
+SELECT CONCAT(first_name, ' ', last_name) AS Employee, emp_no, MIN(salary)
+FROM salaries
+JOIN employees
+USING (emp_no)
+GROUP BY emp_no;
+
     -- Q8F: Find the standard deviation of salaries for each employee.
-	
+SELECT emp_no, MIN(salary) AS 'MIN Salary', MAX(salary), ROUND(STD(salary),0), hire_date
+FROM salaries
+JOIN employees
+USING (emp_no)
+GROUP BY emp_no;
+
     -- Q8G: Find the max salary for each employee where that max salary is greater than $150,000.
-	
+select emp_no, max(salary) 
+from salaries
+GROUP BY emp_no
+HAVING MAX(salary) > 150000
+ORDER BY MAX(salary) DESC;
+
     -- Q8H: Find the average salary for each employee where that average salary is between $80k and $90k.
-
-
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS Employee, ROUND(AVG(salary))
+FROM salaries
+JOIN employees
+USING (emp_no)
+GROUP BY emp_no
+HAVING AVG(salary)
+BETWEEN 80000 AND 90000;
 
 
